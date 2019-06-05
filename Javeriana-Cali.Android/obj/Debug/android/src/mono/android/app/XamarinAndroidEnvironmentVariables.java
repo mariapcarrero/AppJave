eef021b3a0dcf21ecb2a65f583d6cf3538873db3
maria <mpcarrero1@gmail.com>
2019-06-05 13:53:29 -0500

    Changed Fonts Globally

--- Javeriana-Cali.Android/obj/Debug/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ Javeriana-Cali.Android/obj/Debug/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "96975e22-3641-40c8-bec5-46e0743a9277",
+		"XAMARIN_BUILD_ID", "9241da72-987b-463b-a88e-51f2444cf9e6",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
